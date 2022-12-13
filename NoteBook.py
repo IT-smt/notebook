7060a0ad5ea1664fb67787a92512a19cd1cd4a4e
Виктор <smt_razrab@mail.ru>
2022-12-13 21:44:42 +0500

    NoteBook v2.0

--- NoteBook.py
+++ NoteBook.py
@@ -1,9 +1,8 @@
 import sys
-
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QAction, QFont, QIcon
 from PyQt6.QtWidgets import QWidget, QMainWindow, QApplication, QTextEdit, QMenuBar, QPushButton, QVBoxLayout, \
-    QFileDialog, QMessageBox
+    QFileDialog, QMessageBox, QListWidget, QCheckBox, QHBoxLayout, QLabel
 
 
 class Window(QMainWindow):
@@ -13,15 +12,18 @@ class Window(QMainWindow):
         super().__init__()
         self.src = None
         self.file_name = None
+        self.bold = 400
+        self.cursive = False
         self.text_edit = QTextEdit(self)
         self.font = "Sans serif"
         self.font_size = 14
-        self.text_edit.setFont(QFont(self.font, self.font_size))
+        self.set_font()
         self.init_ui()
 
     def init_ui(self):
         """Front."""
         self.setWindowTitle("Блокнот")
+        self.setWindowIcon(QIcon("1.png"))
         self.resize(1200, 720)
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
@@ -41,58 +43,11 @@ class Window(QMainWindow):
 
     def file_menu(self, file_menu):
         """Добавляет кнопки в file_menu."""
-        button_save = QAction("Сохранить", self)
-        button_save.setShortcut("F1")
-        button_save.setIcon(QIcon.fromTheme("document-save"))
-        button_save.triggered.connect(self.save_file)
-        button_save_how = QAction("Сохранить как", self)
-        button_save_how.setShortcut("F2")
-        button_save_how.setIcon(QIcon.fromTheme("document-save-as"))
-        button_save_how.triggered.connect(self.save_file_how)
-        button_open = QAction("Открыть", self)
-        button_open.setShortcut("F3")
-        button_open.setIcon(QIcon.fromTheme("document-open"))
-        button_open.triggered.connect(self.open_file)
-        button_close = QAction("Закрыть", self)
-        button_close.setShortcut("F4")
-        button_close.setIcon(QIcon.fromTheme("application-exit"))
-        button_close.triggered.connect(self.close)
-        file_menu.addAction(button_save)
-        file_menu.addAction(button_save_how)
-        file_menu.addAction(button_open)
+        self.create_action(file_menu, "Сохранить", "F1", self.save_file, "document-save")
+        self.create_action(file_menu, "Сохранить как", "F2", self.save_file_how, "document-save-as")
+        self.create_action(file_menu, "Открыть", "F3", self.open_file, "document-open")
         file_menu.addSeparator()
-        file_menu.addAction(button_close)
-
-    def edit_menu(self, edit_menu):
-        """Добавляет кнопки в edit_menu."""
-        font_size_menu = edit_menu.addMenu("Размер шрифта...")
-        self.font_size_menu(font_size_menu)
-        edit_menu.addSeparator()
-        button_clear = QAction("Очистить", self)
-        button_clear.setShortcut("F8")
-        button_clear.setIcon(QIcon.fromTheme("edit-clear"))
-        button_clear.triggered.connect(self.clear)
-        edit_menu.addAction(button_clear)
-
-    def font_size_menu(self, font_size_menu):
-        """Добавляет кнопки в font_size_menu."""
-        font_size_menu.setIcon(QIcon.fromTheme("zoom-fit-best"))
-        button_plus = QAction("Увеличить", self)
-        button_plus.setShortcut("F5")
-        button_plus.setIcon(QIcon.fromTheme("zoom-in"))
-        button_plus.triggered.connect(self.font_size_plus)
-        font_size_menu.addAction(button_plus)
-        button_minus = QAction("Уменьшить", self)
-        button_minus.setShortcut("F6")
-        button_minus.setIcon(QIcon.fromTheme("zoom-out"))
-        button_minus.triggered.connect(self.font_size_minus)
-        font_size_menu.addAction(button_minus)
-        font_size_menu.addSeparator()
-        button_reset = QAction("Сбросить", self)
-        button_reset.setShortcut("F7")
-        button_reset.setIcon(QIcon.fromTheme("zoom-original"))
-        button_reset.triggered.connect(self.font_size_reset)
-        font_size_menu.addAction(button_reset)
+        self.create_action(file_menu, "Закрыть", "F4", self.close, "application-exit")
 
     def save_file(self):
         """Сохраняет файл."""
@@ -123,32 +78,106 @@ class Window(QMainWindow):
                     self.src = f.read()
                 self.text_edit.setText(self.src)
             except UnicodeDecodeError:
-                message_box = QMessageBox()
-                message_box.setIcon(QIcon)
-                message_box.setText("Не поддерживаемое расширение!")
-                message_box.setWindowTitle("Ошибка")
-                message_box.setStandardButtons(QMessageBox.StandardButton.Ok)
-                message_box.show()
-                message_box.exec()
+                self.message_box("Ошибка", "Не поддерживаемое расширение!", QMessageBox.StandardButton.Ok)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """Выводит всплывающее окно и закрывает приложение."""
+        if self.text_edit.toPlainText() != "":
+            if self.message_box("Сохранение", "Вы хотите сохранить изменения перед выходом?", QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No) == QMessageBox.StandardButton.Yes:
+                self.save_file()
+        sys.exit()
+
+    def message_box(self, title, text, buttons):
+        message_box = QMessageBox()
+        message_box.setText(text)
+        message_box.setWindowTitle(title)
+        message_box.setStandardButtons(buttons)
+        message_box.show()
+        return message_box.exec()
+
+    def edit_menu(self, edit_menu):
+        """Добавляет кнопки в edit_menu."""
+        font_size_menu = edit_menu.addMenu("Размер шрифта...")
+        self.font_size_menu(font_size_menu)
+        font_menu = edit_menu.addMenu("Шрифт...")
+        self.font_menu(font_menu)
+        edit_menu.addSeparator()
+        self.create_action(edit_menu, "Очистить", "F10", self.clear, "edit-clear")
+
+    def font_size_menu(self, font_size_menu):
+        """Добавляет кнопки в font_size_menu."""
+        font_size_menu.setIcon(QIcon.fromTheme("zoom-fit-best"))
+        self.create_action(font_size_menu, "Увеличить", "F5", self.font_size_plus, "zoom-in")
+        self.create_action(font_size_menu, "Уменьшить", "F6", self.font_size_minus, "zoom-out")
+        font_size_menu.addSeparator()
+        self.create_action(font_size_menu, "Сбросить", "F7", self.font_size_reset, "zoom-original")
 
     def font_size_plus(self):
         """Увеличивает размер шрифта на 1."""
         if self.font_size < 1000:
             self.font_size += 1
-        self.text_edit.setFont(QFont(self.font, self.font_size))
+        self.set_font()
 
     def font_size_minus(self):
         """Уменьшает размер шрифта на 1."""
         if self.font_size > 1:
             self.font_size -= 1
-        self.text_edit.setFont(QFont(self.font, self.font_size))
+        self.set_font()
 
     def font_size_reset(self):
         """Изменяет размер шрифта на размер по умолчанию."""
         self.font_size = 14
-        self.text_edit.setFont(QFont(self.font, self.font_size))
+        self.set_font()
+
+    def font_menu(self, font_menu):
+        """Добавляет checkbox'ы в font_menu."""
+        font_menu.setIcon(QIcon.fromTheme("preferences-desktop-font"))
+        self.button_bold = QAction("Жирный", self)
+        self.add_action_methods(font_menu, self.button_bold, "F8", True, "preferences-desktop-font", self.font_bold)
+        font_menu.addSeparator()
+        self.button_cursive = QAction("Курсив", self)
+        self.add_action_methods(font_menu, self.button_cursive, "F9", True, "preferences-desktop-font", self.font_cursive)
+
+    def create_action(self, menu, name, key, func, icon):
+        """Создает action."""
+        button = QAction(name, self)
+        button.setShortcut(key)
+        button.setIcon(QIcon.fromTheme(icon))
+        button.triggered.connect(func)
+        menu.addAction(button)
+
+    def add_action_methods(self, menu, button, key, check, icon, func):
+        button.setShortcut(key)
+        button.setCheckable(check)
+        button.setIcon(QIcon.fromTheme(icon))
+        button.triggered.connect(func)
+        menu.addAction(button)
+
+    def font_bold(self):
+        """Изменяет жирность текста."""
+        if self.button_bold.isChecked():
+            self.button_bold.setCheckable(True)
+            self.bold = 800
+        else:
+            self.button_bold.setCheckable(True)
+            self.bold = 400
+        self.set_font()
+
+    def font_cursive(self):
+        """Изменяет курсив текста."""
+        if self.button_cursive.isChecked():
+            self.button_cursive.setCheckable(True)
+            self.cursive = True
+        else:
+            self.button_cursive.setCheckable(True)
+            self.cursive = False
+        self.set_font()
+
+    def set_font(self):
+        """Ставит шрифт."""
+        self.text_edit.setFont(QFont(self.font, self.font_size, self.bold, self.cursive))
 
     def clear(self):
         """Очищает поле ввода."""
@@ -171,20 +200,11 @@ class Window(QMainWindow):
         elif event.key() == Qt.Key.Key_F7:
             self.font_size_reset()
         elif event.key() == Qt.Key.Key_F8:
+            self.font_bold()
+        elif event.key() == Qt.Key.Key_F9:
+            self.font_cursive()
+        elif event.key() == Qt.Key.Key_F10:
             self.clear()
-
-    def close(self):
-        """Выводит всплывающее окно и закрывает приложение."""
-        if self.text_edit.toPlainText() != "":
-            message_box = QMessageBox()
-            message_box.setText("Вы хотите сохранить изменения перед выходом?")
-            message_box.setWindowTitle("Сохранение")
-            message_box.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
-            message_box.show()
-            result = message_box.exec()
-            if result == QMessageBox.StandardButton.Yes:
-                self.save_file()
-        sys.exit()
 
     def closeEvent(self, event):
         """Срабатывает, когда закрывается приложение."""
